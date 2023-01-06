62c77246520a13beff25c3916f95838d62f30b1c
AlonsoErnesto <ernesto134alonso123@gmail.com>
2023-01-05 21:56:29 -0500

    page nuevocliente

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Layout from "../components/Layout";
 import { gql, useQuery } from '@apollo/client';
-
+import { useRouter } from "next/router";
+import Link from 'next/link';
 
 const OBTENER_CLIENTES_USUARIO = gql`
   query obtenerClientesVendedor {
@@ -16,19 +17,24 @@ const OBTENER_CLIENTES_USUARIO = gql`
 
 export default function Home() {
 
+  const router = useRouter();
   //Consulta de apollo
   const { data, loading, error} = useQuery(OBTENER_CLIENTES_USUARIO);
-  console.log(data);
-  console.log(loading);
-  console.log(error);
 
-  if(loading) return 'Cargando ....'
+  if(loading) return 'Cargando ....';
+
+  if(!data.obtenerClientesVendedor) {
+    return router.push('/login');
+  }
 
   return (
     <>
       <div>
         <Layout>
           <h1 className="text-2xl text-gray-800 font-light">Clientes</h1>
+          <Link href="/nuevocliente" legacyBehavior>
+            <a className="bg-blue-800 py-2 px-5 mt-3 inline-block text-white rounded text-sm hover:bg-gray-800 mb-3 uppercase font-bold">Nuevo Cliente</a>
+          </Link>
           <table className="table-auto shadow-md mt-10 w-full w-lg">
             <thead className="bg-gray-800">
               <tr className="text-white">
---
+++ pages/nuevocliente.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+const NuevoCliente = () => {
+   return (
+      <Layout>
+         <h1 className='text-2xl text-gray-800 font-light'>Nuevo Cliente</h1>
+      </Layout>
+   )
+}
+
+export default NuevoCliente
